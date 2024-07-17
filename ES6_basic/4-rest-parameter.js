51446ee7a5a2265519b5ace86fd9b406dd6bcc3f
TonyHolby <tonynemoutheholberton@gmail.com>
2024-07-17 22:57:47 +0200

    Test doc in 4-rest-parameter.js

--- ES6_basic/4-rest-parameter.js
+++ ES6_basic/4-rest-parameter.js
@@ -1,3 +1,9 @@
+/**
+ * A function to return the number of arguments passed to it
+ * using the rest parameter syntax.
+ * @param  {...any} args 
+ * @returns 
+ */
 export default function returnHowManyArguments(...args) {
     return args.length;
 }
