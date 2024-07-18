dff50e58a58bd60d2986d9ec6ea26fa4b43a00bd
TonyHolby <tonynemoutheholberton@gmail.com>
2024-07-18 12:13:16 +0200

    Fix indent in 7-getBudgetObject.js

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,9 +1,9 @@
 /**
  * A function that returns the object budget in using simply
  * the object property value shorthand syntax.
- * @param {string} income 
- * @param {string} gdp 
- * @param {string} capita 
+ * @param {string} income
+ * @param {string} gdp
+ * @param {string} capita
  * @returns the object budget.
  */
 export default function getBudgetObject(income, gdp, capita) {
