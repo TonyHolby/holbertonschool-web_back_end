27d830b0be0f29a812714fed82b467487e98d1c0
TonyHolby <tonynemoutheholberton@gmail.com>
2024-07-18 10:05:16 +0200

    Write code in 7-getBudgetObject.js

---
+++ ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,17 @@
+/**
+ * A function that returns the object budget in using simply
+ * the object property value shorthand syntax.
+ * @param {string} income 
+ * @param {string} gdp 
+ * @param {string} capita 
+ * @returns 
+ */
+export default function getBudgetObject(income, gdp, capita) {
+    const budget = {
+        income,
+        gdp,
+        capita,
+    };
+
+    return budget;
+}
