6cf551556429249212a7ca1f9f2616fcae0481b0
TonyHolby <tonynemoutheholberton@gmail.com>
2024-07-22 16:55:26 +0200

    Fix code in 8-clean_set.js

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,8 @@
 function cleanSet(set, startString) {
+  if (startString === '') {
+    return '';
+  }
+
   return Array.from(set).filter((value) => value.startsWith(startString)).map((value) => value.slice(startString.length)).join('-');
 }
 
