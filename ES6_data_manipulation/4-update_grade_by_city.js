04d9ff10c08a248424638cad92007199b824cb51
TonyHolby <tonynemoutheholberton@gmail.com>
2024-07-22 14:18:10 +0200

    Write code in 4-update_grade_by_city.js

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+function updateStudentGradeByCity(students, city, newGrades) {
+  return students.filter((student) => student.location === city).map((student) => {
+    const studentGrade = newGrades.find((grade) => grade.studentId === student.id);
+    return {
+      ...student,
+      grade: studentGrade ? studentGrade.grade : 'N/A',
+    };
+  });
+}
+
+export default updateStudentGradeByCity;
