f49d54a41f12bf0f640764eaf4063638e8a1305a
TonyHolby <tonynemoutheholberton@gmail.com>
2024-07-23 10:28:26 +0200

    Fix code in 0-promise.js

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,5 +1,5 @@
 function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     setTimeout(() => {
       const success = Math.random() > 0.5;
 
