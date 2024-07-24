8dbaa6a28906ef89af87b5da7dab0047637fcce2
TonyHolby <tonynemoutheholberton@gmail.com>
2024-07-24 17:48:38 +0200

    Fix code in 6-final-user.js

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,17 +1,19 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   const signUpPromise = signUpUser(firstName, lastName);
-  const uploadPromise = uploadPhoto(fileName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
 
-  return Promise.allSettled([signUpPromise, uploadPromise])
-    .then((results) => results.map((promisesettled) => {
-      if (promisesettled.status === 'fulfilled') {
-        return { status: promisesettled.status, value: promisesettled.value };
-      }
-      return { status: promisesettled.status, value: promisesettled.reason };
-    }));
+  const promisesettled = await Promise.allSettled([signUpPromise, uploadPhotoPromise]);
+
+  return promisesettled.map((promises) => {
+    const result = { status: promises.status };
+    if (promises.status === 'fulfilled') {
+      result.value = promises.value;
+    } else {
+      result.value = promises.reason;
+    }
+    return result;
+  });
 }
-
-export default handleProfileSignup;
