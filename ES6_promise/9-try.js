e86a374107f54b555d33ef79e2d4812d04ead030
TonyHolby <tonynemoutheholberton@gmail.com>
2024-07-24 23:50:14 +0200

    Write code in 9-try.js

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,11 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push('Error: cannot divide by 0');
+  }
+  queue.push('Guardrail was processed');
+  return queue;
+}
