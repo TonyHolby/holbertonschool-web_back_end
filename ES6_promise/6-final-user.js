cbaa2aaf368bfe0d74fab34112d274aa7022a4e8
TonyHolby <tonynemoutheholberton@gmail.com>
2024-07-24 16:31:10 +0200

    Write code in 7-load_balancer.js

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,17 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUpPromise, uploadPromise])
+    .then((results) => results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return { status: result.status, value: result.value };
+      }
+      return { status: result.status, reason: result.reason.toString() };
+    }));
+}
+
+export default handleProfileSignup;
