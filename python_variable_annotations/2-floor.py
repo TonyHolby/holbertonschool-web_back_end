9516e570ec90e57d7aa0f7c27014c02d710a04ce
TonyHolby <tonynemoutheholberton@gmail.com>
2024-08-13 16:31:18 +0200

    Write code in 2-floor.py

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+    Defines a method named floor that takes a float n as argument
+    and returns the floor of the float.
+"""
+
+import math
+
+def floor(n: float) -> int:
+    """ Returns the floor of the float. """
+    return math.floor(n)
