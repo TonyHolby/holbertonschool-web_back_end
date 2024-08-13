c672ceea8897f4fb6a75efdd9d09fe493abb11f6
TonyHolby <tonynemoutheholberton@gmail.com>
2024-08-13 22:28:32 +0200

    Fix pycodestyle in 8-make_multiplier.py

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -5,6 +5,7 @@
 """
 from typing import Callable
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """ Defines a function that multiplies a float by multiplier. """
     def multiplier_function(x: float) -> float:
