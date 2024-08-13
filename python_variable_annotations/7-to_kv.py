325c4bd3a9c85425660bc179141e3c6e72ac6610
TonyHolby <tonynemoutheholberton@gmail.com>
2024-08-13 22:11:34 +0200

    Write code in 7-to_kv.py

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+    Defines a method named sum_mixed_list that takes a string k
+    and an int or float v as arguments and returns a tuple.
+"""
+from typing import Union, Tuple
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """ Returns a tuple. """
+    return k, float(v ** 2)
