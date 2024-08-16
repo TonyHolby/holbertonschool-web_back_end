4b3baa61b3590ca8ff7643011b5ee07c687013d8
TonyHolby <tonynemoutheholberton@gmail.com>
2024-08-16 17:24:33 +0200

    Fix code in 3-tasks.py

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -18,4 +18,6 @@ def task_wait_random(max_delay: int) -> float:
         Returns:
             A asyncio.Task.
     """
-    return asyncio.create_task(wait_random(max_delay))
+    asyncio.Task = asyncio.create_task(wait_random(max_delay))
+
+    return asyncio.Task
