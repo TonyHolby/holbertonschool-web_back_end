e5ddd8e4981d9d0d4b44d48ba7648055871b770a
TonyHolby <tonynemoutheholberton@gmail.com>
2024-08-23 16:12:09 +0200

    Fix typing annotations in 0-async_generator.py

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """ Defines a coroutine named async_generator that takes no arguments. """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
         The coroutine loops 10 times, each time asynchronously wait 1
         second, then yield a random number between 0 and 10.
