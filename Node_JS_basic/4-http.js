e8b37f64c343252e6fc3aed084563f20e63d1572
TonyHolby <tonynemoutheholberton@gmail.com>
2024-09-17 10:57:21 +0200

    Fix indent in 4-http.js

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,13 +1,13 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
 
-    res.end('Hello Holberton School!');
+  res.end('Hello Holberton School!');
 });
 
 app.listen(1245, () => {
-    console.log('Server is listening on port 1245');
+  console.log('Server is listening on port 1245');
 });
 
 module.exports = app;
