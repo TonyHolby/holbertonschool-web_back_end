ba59437fd03cacdba2b56d500109e5d83f35f178
TonyHolby <tonynemoutheholberton@gmail.com>
2024-09-17 11:22:44 +0200

    Fix code and indent in 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,24 +1,24 @@
 const readline = require('readline');
 
-const name_input = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+const nameInput = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
 console.log('Welcome to Holberton School, what is your name?');
 
-name_input.on('line', (input) => {
-    console.log(`Your name is: ${input}`);
-    name_input.close();
+nameInput.on('line', (input) => {
+  console.log(`Your name is: ${input}`);
+  nameInput.close();
 });
 
 if (process.stdin.isTTY) {
-    name_input.on('close', () => {
-        process.exit(0);
-    });
+  nameInput.on('close', () => {
+    process.exit(0);
+  });
 } else {
-    name_input.on('close', () => {
-        console.log('This important software is now closing');
-        process.exit(0);
-    });
+  nameInput.on('close', () => {
+    console.log('This important software is now closing');
+    process.exit(0);
+  });
 }
