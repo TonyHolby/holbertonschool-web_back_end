3be86482946c6f572919ec77b302237f20a0db3e
TonyHolby <tonynemoutheholberton@gmail.com>
2024-09-19 10:50:05 +0200

    Fix indent in 5-http.js

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const url = require('url');
 const countStudents = require('./3-read_file_async');
+
 const database = process.argv[2];
 
 const app = http.createServer((req, res) => {
