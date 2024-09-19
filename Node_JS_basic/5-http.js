e63f58fb551e83421fb09f4634ca7b2eb7dcebf1
TonyHolby <tonynemoutheholberton@gmail.com>
2024-09-19 10:47:50 +0200

    Write code in 5-http.js

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const url = require('url');
+const countStudents = require('./3-read_file_async');
+const database = process.argv[2];
+
+const app = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+
+  if (parsedUrl.pathname === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  } else if (parsedUrl.pathname === '/students') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    countStudents(database)
+      .then((output) => {
+        res.end(`This is the list of our students\n${output}`);
+      })
+      .catch((err) => {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end(err.message);
+      });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+});
+
+app.listen(1245, () => {
+  console.log('Server is listening on port 1245');
+});
+
+module.exports = app;
