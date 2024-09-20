ac6d436a6ecc3b2c9635a60639e6708b24afbb8b
TonyHolby <tonynemoutheholberton@gmail.com>
2024-09-20 14:16:07 +0200

    Write code in 7-http_express.js

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const port = 1245;
+const database = process.argv[2];
+
+app.get('/', (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  countStudents(database)
+    .then((output) => {
+      res.send(`This is the list of our students\n${output}`);
+    })
+    .catch((err) => {
+      res.send(`This is the list of our students\n${err.message}`);
+    });
+});
+
+app.listen(port, () => {
+  console.log('...');
+});
+
+module.exports = app;
